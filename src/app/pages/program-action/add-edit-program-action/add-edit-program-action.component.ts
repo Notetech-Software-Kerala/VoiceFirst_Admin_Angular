2430ee703cf19eed81b1ec9707bbc9dfbc9b32bc
athulks-notetech <athulks@notetech.com>
2026-01-20 19:53:43 +0530

    uninstalled cypress

---
+++ src/app/pages/program-action/add-edit-program-action/add-edit-program-action.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-program-action',
+  standalone: false,
+  templateUrl: './add-edit-program-action.component.html',
+  styleUrl: './add-edit-program-action.component.css',
+})
+export class AddEditProgramActionComponent {
+
+}
