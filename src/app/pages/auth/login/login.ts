65bc8fc8a2a46c9e059a23d377d258aa49cfbb67
athulks-notetech <athulks@notetech.com>
2026-03-06 18:24:28 +0530

    update

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -74,7 +74,7 @@ export class Login {
         clientType: 'Web',
         device: {
           deviceID: rawDevice.deviceID,
-          version: 1,
+          version: this.APP_VERSION,
           deviceName: rawDevice.deviceName,
           deviceType: rawDevice.deviceType,
           os: rawDevice.os,
