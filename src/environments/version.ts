d14219444ef21a1cc2c5b2a3660542be06dfb291
athulks-notetech <athulks@notetech.com>
2026-03-24 15:01:31 +0530

    update- login page

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit manually.
 export const APP_VERSION = '1.0.0';
-export const BUILD_TIME = '2026-03-06T08:02:28.489Z';
+export const BUILD_TIME = '2026-03-24T09:24:11.978Z';
